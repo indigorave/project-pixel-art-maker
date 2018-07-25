6583bd82a07a4cc85b59ba71898d92e57d4617b8
indigorave <37487229+indigorave@users.noreply.github.com>
2018-07-25 16:17:05 -0400

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,10 +1,29 @@
-// Select color input
-// Select size input
+$(document).ready(function(){
+  $('#sizePicker').submit(function makeGrid(grid) {
+    $('table tr').remove();
+        let rows = $('#inputHeight').val(); //input number of rows
+        let columns = $('#inputWidth').val(); //input number of columns
+           for (let m = 1; m <= rows; m++){
+              $('table').append('<tr></tr>');
+              $('tr').css('height');
+                for (let n = 1; n <= columns; n++) {
+                  $('tr:last').append('<td></td>');
+                  $('td').css('width');
+                  $('td').attr('class','cells');
+              }
+        }
+    grid.preventDefault();
 
-// When size is submitted by the user, call makeGrid()
+    $("#pixel_canvas").on("mousedown mouseover", "td", function(e){
+        if (e.buttons === 1){
+          $(this).css("background-color", $("#colorPicker").val());
+        }
+    });
 
-function makeGrid() {
+ $('.cells').contextmenu(function(event){
+     $(event.target).css('background-color', 'white')
+   return false;
+       });
+     });
+});
 
-// Your code goes here!
-
-}
